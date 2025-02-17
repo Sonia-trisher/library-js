b66860ff7e926520a7aa34ddb0c2321a8035e4a7
Sonia-trisher <soniatrisher86@gmail.com>
2025-02-17 05:38:07 -0800

    yh

---
+++ index.js
@@ -0,0 +1,12 @@
+const library =[
+    {
+        id:'245vgr',
+        title:'to killbird',
+        authour:'sonia',
+        avilability:'available',
+        borrowedId:'245vgr',
+        dateBorrowed:'2023-09-01',
+        dateReturned:'2023-09-15',
+
+    }
+]
