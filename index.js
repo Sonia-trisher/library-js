bdc800fd2a852ea3fe2b71809ae89f5a9dcbe480
Sonia-trisher <soniatrisher86@gmail.com>
2025-02-17 11:32:19 -0800

    morebooks

--- index.js
+++ index.js
@@ -7,6 +7,75 @@ const library =[
         borrowedId:'245vgr',
         dateBorrowed:'2023-09-01',
         dateReturned:'2023-09-15',
-
+       createAt:'2023-09-01',
+       upDatedAt:'2023-09-01',
+    },
+    {
+        id:'245vgr',
+        title:'to killbird',
+        authour:'sonia',
+        avilability:'available',
+        borrowedId:'245vgr',
+        dateBorrowed:'2023-09-01',
+        dateReturned:'2023-09-15',
+       createAt:'2023-09-01',
+       upDatedAt:'2023-09-01',
+    },
+    {
+        id:'245vgr',
+        title:'to killbird',
+        authour:'sonia',
+        avilability:'available',
+        borrowedId:'245vgr',
+        dateBorrowed:'2023-09-01',
+        dateReturned:'2023-09-15',
+       createAt:'2023-09-01',
+       upDatedAt:'2023-09-01',
     }
-]
+];
+ 
+const Users =[ 
+    {
+        id:'245vgr',
+        name:'tessa',
+        email:'tessy@gmai.com',
+        borrowedBooks:[]
+    },
+    
+    {
+        id:'235vgr',
+        name:'sonia',
+        email:'wamahorosonia1@gmai.com',
+        borrowedBooks:'to killbird'
+    },
+    
+    {
+        id:'225vgr',
+        name:'keza',
+        email:'keza@gmai.com',
+        borrowedBooks:'to killbird'
+    },
+    
+
+];
+
+
+function  addBooks(){
+
+}   
+
+function addManyBooks(){
+
+}
+const UpdateBook = () => {
+
+}
+const RemoveBook =() => {
+
+}
+const SearchBook = () => {
+
+}
+const UpdateBook = () => {
+
+}
