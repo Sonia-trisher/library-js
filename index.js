5e8eb8fc50f4530e1381afbef1c4cfec7bc1885e
Sonia-trisher <soniatrisher86@gmail.com>
2025-02-18 03:58:39 -0800

    mistake on boolean

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const library =[
         title:'to killbird',
         authour:'sonia',
         genre: 'classsic',
-       available:'false',
+        available:false,
         borrowedId:'245vgr',
         dateBorrowed:'2023-09-01',
         dateReturned:'2023-09-15',
@@ -16,7 +16,7 @@ const library =[
         title:'cl',
         authour:'dct.liliane',
         genre: 'classsic',
-        available:'true',
+        available:true,
         borrowedId:'245vgr',
         dateBorrowed:'2023-09-01',
         dateReturned:'2023-09-15',
@@ -28,7 +28,7 @@ const library =[
         title:'1978',
         authour:'dj.eric',
         genre: 'classsic',
-       available:'false',
+        available:false,
         borrowedId:'245vgr',
         dateBorrowed:'2023-09-01',
         dateReturned:'2023-09-15',
@@ -40,7 +40,7 @@ const library =[
         title:'mamamia',
         authour:'hardin',
         genre: 'classsic',
-       available:'false',
+        available:false,
         borrowedId:'245vgr',
         dateBorrowed:'2023-09-01',
         dateReturned:'2023-09-15',
@@ -52,7 +52,7 @@ const library =[
         title:'my love',
         authour:'dj.sss',
         genre: 'classsic',
-       available:'true',
+        available:true,
         borrowedId:'245vgr',
         dateBorrowed:'2023-09-01',
         dateReturned:'2023-09-15',
@@ -90,7 +90,7 @@ const Users =[
 
 function  addBooks(){
 
-}   
+}  
 
 function addManyBooks(){
 
@@ -104,23 +104,38 @@ const RemoveBook =() => {
 const SearchBook = () => {
 
 }
-
-function DisplayBooks (){
-    return library;
-}
-// function DisplayAvailableBooks(isAvailable){
-//     var books = [];
-//     if(isAvailable){
-//        books = library.filter(book => book.available)
-//     }else{
-//         books = library.filter(book => !book.available)
-//     }
-//   return books;
-    
+// function DisplayBooks (){
+//     return library;
 // }
+ function DisplayAvailableBooks(available){
+    var books = [];
+    if(available){
+       books = library.filter(book => book.available)
+    }else{
+        books = library.filter(book => !book.available)
+    }
+  return books;
+    
+}
+
 
    
-console.log(DisplayBooks())
-console.log("hello");
+// console.log(DisplayBooks())
+// console.log(DisplayAvailableBooks(false))
+console.log(DisplayAvailableBooks(true))
+const newBook ={
+    id:'250vgr',
+    title:'The lord of the right',
+    authour:'dct.liliane',
+    genre: 'fantastic',
+    available:true,
+    borrowedId:'245vgr',
+    dateBorrowed:'2023-09-01',
+    dateReturned:'2023-09-15',
+   createAt:'2023-09-01',
+   upDatedAt:'2023-09-01',
+
+}
+
 
 
